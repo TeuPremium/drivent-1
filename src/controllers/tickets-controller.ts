e309c49dcf12929edaa251edf760b2b197f83dee
Vini <vinicius.brandaot@gmail.com>
2023-04-16 20:01:42 -0300

    post controller

--- src/controllers/tickets-controller.ts
+++ src/controllers/tickets-controller.ts
@@ -1,9 +1,11 @@
+import { strict } from 'assert';
 import { NextFunction, Request, Response } from 'express';
 import httpStatus from 'http-status';
 import ticketRepository from '@/repositories/ticket-repository';
 import ticketServices from '@/services/ticket-services';
+import { invalidDataError } from '@/errors';
 
-async function getTicketTypes(req: Request, res: Response, next: NextFunction) {
+async function getTicketTypes(req: Request, res: Response) {
   try {
     const response = await ticketRepository.allTypes();
     return res.status(httpStatus.OK).send(response);
@@ -13,7 +15,7 @@ async function getTicketTypes(req: Request, res: Response, next: NextFunction) {
   }
 }
 
-async function getTickets(req: Request, res: Response, next: NextFunction) {
+async function getTickets(req: Request, res: Response) {
   try {
     const { userId } = res.locals;
     const response = await ticketServices.findTicket(userId);
@@ -21,6 +23,16 @@ async function getTickets(req: Request, res: Response, next: NextFunction) {
     return res.status(httpStatus.OK).send(response);
   } catch (error) {
     return res.status(404).send(error);
+  }
+}
+
+async function postTickets(req: Request, res: Response) {
+  try {
+    const { ticketTypeId }: { ticketTypeId: number } = req.body;
+    console.log(ticketTypeId);
+    res.sendStatus(httpStatus.CREATED);
+  } catch (error) {
+    throw invalidDataError([`invalid ticket format`]);
   }
 }
 
--- src/services/ticket-services/index.ts
+++ src/services/ticket-services/index.ts
@@ -1,24 +1,17 @@
-import { notFoundError } from '@/errors';
+import { invalidDataError, notFoundError } from '@/errors';
 import ticketRepository from '@/repositories/ticket-repository';
 
 async function findTicket(userId: number) {
   try {
-    console.log(userId);
     const { id } = await ticketRepository.userEnrollment(userId);
     if (!id) {
       throw notFoundError();
     }
-
-    console.log('enrollment');
-    console.log(id);
-
     const ticket = await ticketRepository.userTicket(userId);
     const { Ticket } = ticket;
     if (!Ticket[0]) {
       throw notFoundError();
     }
-    console.log('Ticket');
-    console.log(Ticket);
 
     return Ticket[0];
   } catch (error) {
@@ -26,6 +19,15 @@ async function findTicket(userId: number) {
   }
 }
 
+async function orderTicket(ticketTypeId: number) {
+  try {
+    console.log(ticketTypeId);
+  } catch (error) {
+    throw invalidDataError([`invalid ticket type`]);
+  }
+}
+
 export default {
   findTicket,
+  orderTicket,
 };
