e7a052da4df499d6a02b7baa56e692db23584e0e
Vini <vinicius.brandaot@gmail.com>
2023-04-21 22:30:36 -0300

    generating valid bodies for hotel validation

--- tests/integration/hotels.test.ts
+++ tests/integration/hotels.test.ts
@@ -1,21 +1,34 @@
 import supertest from 'supertest';
+import faker from '@faker-js/faker';
 import { createHotel, createRoom } from '../factories';
 import { cleanDb } from '../helpers';
 import { prisma } from '@/config';
 import app, { init } from '@/app';
 
 beforeAll(async () => {
-  await cleanDb();
   await init();
+  await cleanDb();
 });
 
 const server = supertest(app);
 
 describe('GET /hotels', () => {
+  const generateValidHotel = () => ({
+    name: faker.company.companyName(),
+    image: faker.image.city(),
+  });
+
+  const generateValidRoom = (hotelId: number) => ({
+    name: faker.name.firstName(),
+    hotelId,
+    capacity: Math.floor(Math.random() * 10),
+  });
+
   it('should respond with all listed hotels', async () => {
-    console.log(prisma);
     // const getHotels = await server.get('/hotels');
-    // const hotel = createHotel('prainha hotel', faker.image.city());
+    const hotel = generateValidHotel();
+    // const rooms = await createRoom(, )
+    console.log(hotel);
     // expect(getHotels.body).toMatchObject();
     expect(0).toEqual(0);
   });
