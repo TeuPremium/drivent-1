6e8feb2c700ee13a2b4582335a18bd83e9c9bc37
Vini <vinicius.brandaot@gmail.com>
2023-04-21 22:06:41 -0300

    updated prisma

--- tests/factories/hotels-factory.ts
+++ tests/factories/hotels-factory.ts
@@ -1,7 +1,7 @@
 import { Hotel, Room } from '@prisma/client';
 import { prisma } from '@/config';
 
-export function createRoom(name: string, image: string): Promise<Hotel> {
+export function createHotel(name: string, image: string): Promise<Hotel> {
   return prisma.hotel.create({
     data: {
       name,
@@ -10,7 +10,7 @@ export function createRoom(name: string, image: string): Promise<Hotel> {
   });
 }
 
-export function createHotel(name: string, capacity: number, hotelId: number): Promise<Room> {
+export function createRoom(name: string, capacity: number, hotelId: number): Promise<Room> {
   return prisma.room.create({
     data: {
       name,
--- tests/integration/hotels.test.ts
+++ tests/integration/hotels.test.ts
@@ -1,5 +1,7 @@
 import supertest from 'supertest';
+import { createHotel, createRoom } from '../factories';
 import { cleanDb } from '../helpers';
+import { prisma } from '@/config';
 import app, { init } from '@/app';
 
 beforeAll(async () => {
@@ -11,32 +13,33 @@ const server = supertest(app);
 
 describe('GET /hotels', () => {
   it('should respond with all listed hotels', async () => {
-    const getHotels = await server.get('/hotels');
-
+    console.log(prisma);
+    // const getHotels = await server.get('/hotels');
+    // const hotel = createHotel('prainha hotel', faker.image.city());
     // expect(getHotels.body).toMatchObject();
     expect(0).toEqual(0);
   });
 
-  it('should respond with all listed hotels', async () => {
-    const getHotels = await server.get('/hotels');
+  // it('should respond with all listed hotels', async () => {
+  //   const getHotels = await server.get('/hotels');
 
-    // expect(getHotels.body).toMatchObject();
-    expect(0).toEqual(0);
-  });
+  //   // expect(getHotels.body).toMatchObject();
+  //   expect(0).toEqual(0);
+  // });
 });
 
-describe('GET /hotels/:hotelId', () => {
-  it('should respond with the correct hotel', async () => {
-    const getHotels = await server.get('/hotels');
-
-    // expect(getHotels.body).toMatchObject();
-    expect(0).toEqual(0);
-  });
-
-  it('should respond with correct status', async () => {
-    const getHotels = await server.get('/hotels');
-
-    // expect(getHotels.body).toMatchObject();
-    expect(0).toEqual(0);
-  });
-});
+// describe('GET /hotels/:hotelId', () => {
+// it('should respond with the correct hotel', async () => {
+// const getHotels = await server.get('/hotels');
+//
+// expect(getHotels.body).toMatchObject();
+// expect(0).toEqual(0);
+// });
+//
+// it('should respond with correct status', async () => {
+// const getHotels = await server.get('/hotels');
+//
+// expect(getHotels.body).toMatchObject();
+// expect(0).toEqual(0);
+// });
+// });
