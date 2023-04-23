88c90b1d55bcc9e8b497747a0d2bfe84fbaac274
Vini <vinicius.brandaot@gmail.com>
2023-04-23 10:39:13 -0300

    404 errors added to hotel route

--- src/controllers/hotel-controller.ts
+++ src/controllers/hotel-controller.ts
@@ -1,23 +1,22 @@
 import { Request, Response, NextFunction } from 'express';
 import httpStatus from 'http-status';
 import hotelServices from '@/services/hotel-service';
-import { prisma } from '@/config';
 
 async function getAllHotels(req: Request, res: Response, next: NextFunction) {
   try {
-    const hotelList = await hotelServices.getAllHotels();
+    const { userId } = res.locals;
+    const hotelList = await hotelServices.getAllHotels(userId);
 
-    console.log(hotelList);
-    // return res.status(200).send(hotelList);
     return res.status(httpStatus.OK).send(hotelList);
   } catch (error) {
+    console.log(error);
     next(error);
   }
 }
 
 async function getHotelById(req: Request, res: Response, next: NextFunction) {
   try {
-    const hotel = await hotelServices.getAllHotels();
+    const hotel = await hotelServices.getHotelById;
     return res.status(200).send(hotel);
   } catch (error) {
     next(error);
--- src/services/hotel-service/index.ts
+++ src/services/hotel-service/index.ts
@@ -1,7 +1,25 @@
 import { getHotels } from '@/repositories/hotel-repository';
+import { notFoundError } from '@/errors';
+import ticketRepository from '@/repositories/ticket-repository';
 
-async function getAllHotels() {
-  return getHotels();
+async function getAllHotels(userId: number) {
+  const enrollment = await ticketRepository.userEnrollment(userId);
+  if (!enrollment) {
+    throw notFoundError();
+  }
+  const ticket = await ticketRepository.userTicket(userId);
+  const { Ticket } = ticket;
+  // console.log(ticket);
+  if (!Ticket[0]) {
+    throw notFoundError();
+  }
+  const hotelList = await getHotels();
+
+  if (!hotelList[0]) {
+    throw notFoundError();
+  }
+
+  return hotelList;
 }
 
 async function getHotelById(id: number) {
